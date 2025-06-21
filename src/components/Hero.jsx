db1a525793874355753145943913c590f1559276
TariqKichawele <tariqkichawele66@gmail.com>
2025-06-21 10:20:31 -0400

    Enhance Hero component with video playback and responsive GSAP animations; integrate useRef and useMediaQuery for improved functionality

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all'
-import React from 'react'
+import React, { useRef } from 'react'
 import gsap from 'gsap'
+import { useMediaQuery } from 'react-responsive'
 
 const Hero = () => {
+
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({ maxWidth: 767 });
 
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words'});
@@ -38,6 +42,25 @@ const Hero = () => {
         .to(".right-leaf", { y: 200 }, 0)
         .to(".left-leaf", { y: -200 }, 0)
         .to(".arrow", { y: 100 }, 0);
+
+        const startValue = isMobile ? "top 50%" : "center 60%";
+        const endValue = isMobile ? "120% top" : "bottom top";
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            })
+        }
     }, [])
   return (
     <>
@@ -79,7 +102,13 @@ const Hero = () => {
         </section>
         
         <div className="video absolute inset-0">
-            Video
+            <video
+                ref={videoRef}
+                muted
+                playsInline
+                preload="auto"
+                src="/videos/output.mp4"
+            />
         </div>
 	</>
   )
